e5f15a250238c940fecfd41c4de9d83151d6b9a6
kdh610 <k_dh610@naver.com>
2025-07-19 22:40:15 +0900

    first commit

---
+++ src/main/java/Qraft/newsalert/NewsalertApplication.java
@@ -0,0 +1,13 @@
+package Qraft.newsalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsalertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsalertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Qraft/newsalert/NewsalertApplicationTests.java
@@ -0,0 +1,13 @@
+package Qraft.newsalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsalertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
